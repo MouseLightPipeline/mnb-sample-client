edb11d7550cebd59c961a15a1087174460d551e5
Patrick Edson <pedson@users.noreply.github.com>
2017-10-01 17:51:50 -0400

    Lint

--- src/components/Samples.tsx
+++ src/components/Samples.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {graphql} from 'react-apollo';
+import {graphql} from "react-apollo";
 import gql from "graphql-tag";
 import {GraphQLDataProps} from "react-apollo/lib/graphql";
 import {Grid, Row, Col} from "react-bootstrap";
