e70e9d31ce76ccedd2171d227010fa73975fee14
Patrick Edson <pedson@users.noreply.github.com>
2018-10-07 10:15:01 -0400

    Clean up state management.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# sample-client
+# Sample Client
+* Manage sample and neuron entry from acquisition samples
+* Modify customizable brain compartment properties (aliases)
+
+### Notes
+* Currently stuck in React 15.x and Typescript 2.3.x limbo with dependency on ndb-react-components
+* Apollo client out of date - requires refactor of queries for new API
+* Tree control hard-coded to 2.x due to React 15 dependency
--- src/components/Compartments.tsx
+++ src/components/Compartments.tsx
@@ -3,7 +3,6 @@ import {graphql} from "react-apollo";
 import gql from "graphql-tag";
 
 import {Treebeard} from "react-treebeard";
-// const treebeard = require("react-treebeard");
 
 import {IBrainArea} from "../models/brainArea";
 
@@ -30,85 +29,103 @@ const CompartmentsQuery = gql`query Compartments {
     }
 }`;
 
+interface ICompartmentNode {
+    name: string;
+    toggled: boolean;
+    active: boolean;
+    children: ICompartmentNode[];
+    compartment: IBrainArea;
+}
 
 interface ICompartmentsProps {
     compartmentsQuery?: any;
 }
 
 interface ICompartmentsState {
-    flag?: boolean;
-    data?: any;
+    data?: ICompartmentNode;
+    selectedNode?: ICompartmentNode;
 }
 
 @graphql(CompartmentsQuery, {
     name: "compartmentsQuery",
     options: {
-        pollingInterval: 30000
+        pollingInterval: 60000
     }
 })
 export class Compartments extends React.Component<ICompartmentsProps, ICompartmentsState> {
+    private compartments = new Map<number, ICompartmentNode>();
+
     public constructor(props: ICompartmentsProps) {
         super(props);
 
         this.state = {
-            flag: false,
-            data: null
+            data: null,
+            selectedNode: null
         }
     }
 
-    componentWillReceiveProps(props: ICompartmentsProps) {
+    public componentWillReceiveProps(props: ICompartmentsProps) {
         const {loading, error} = props.compartmentsQuery;
 
-        if (!loading && !error && this.state.data == null) {
+        if (loading || error) {
+            return;
+        }
+
+        if (this.state.data == null) {
             let sorted = props.compartmentsQuery.brainAreas.slice().sort((a: IBrainArea, b: IBrainArea) => {
                 return a.depth - b.depth;
             });
 
-            const parents = new Map<number, any>();
+            const root = sorted[0];
 
-            const data = {
-                name: sorted[0].name,
+            const data: ICompartmentNode = {
+                name: root.name,
                 toggled: true,
-                children: [] as any[]
+                active: true,
+                children: null,
+                compartment: root
             };
 
-            parents.set(sorted[0].structureId, data);
+            this.compartments.set(sorted[0].structureId, data);
 
             sorted = sorted.slice(1);
 
-
             sorted.forEach((c: IBrainArea) => {
-
-                const node = {
+                const node: ICompartmentNode = {
                     name: c.name,
-                    toggled: false
+                    toggled: false,
+                    active: false,
+                    children: null,
+                    compartment: c
                 };
 
-                parents.set(c.structureId, node);
+                this.compartments.set(c.structureId, node);
 
-                const parent = parents.get(c.parentStructureId);
+                const parent = this.compartments.get(c.parentStructureId);
 
                 if (parent) {
                     if (!parent.children) {
-                        parent.children = [] as any[];
+                        parent.children = [];
                     }
                     parent.children.push(node);
-                } else {
-                    console.log(c);
                 }
             });
 
             this.setState({data});
         }
-
     }
 
-    public onToggle = (node: any, toggled: boolean) => {
-        console.log(node);
+    public onToggle = (node: ICompartmentNode, toggled: boolean) => {
+        if (this.state.selectedNode && this.state.selectedNode != node) {
+            this.state.selectedNode.active = false;
+            node.active = true;
+        }
+
         if (node.children) {
             node.toggled = toggled;
         }
-        this.setState({flag: !this.state.flag});
+
+        this.setState({selectedNode: node});
     };
 
     public render() {
